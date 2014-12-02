038a7cb0b7c250e9e37f244814090b0815e2f8de
Ricky Hussmann <ricky.hussmann@gmail.com>
2014-12-01 21:31:48 -0500

    First pass at a file-based streaming cache

--- routes/v1.js
+++ routes/v1.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+var fs = require('fs');
 var gm = require('gm'),
     imageMagick = gm.subClass({ imageMagick: true });
 var bunyan = require('bunyan');
 var log = bunyan.createLogger({name: "apiV1"});
-
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -13,18 +13,28 @@ router.get('/', function(req, res, next) {
   whom = unescape(whom);
   whom = whom.replace(/\+/g, " ");
 
-  imageMagick(__dirname + '/../public/gifs/liquor.gif')
-    .font("Impact.ttf", 70)
-    .stroke("black")
-    .strokeWidth(2)
-    .fill("white")
-    .drawText(0, 0, whom, "center")
-    .stream('gif', function (err, stdout, stderr) {
-      if (err) return next(err);
-      res.set('Content-Type', 'image/gif');
-      stdout.pipe(res);
-      stdout.on('error', next);
-    });
+  var cacheDir = process.env.CACHE_DIR || "gif_cache";
+  var cacheFile = cacheDir + "/" + whom + ".gif";
+  if (!fs.existsSync(cacheFile)) {
+    var file = fs.createWriteStream(cacheFile);
+    imageMagick(__dirname + '/../public/gifs/liquor.gif')
+      .font("Impact.ttf", 70)
+      .stroke("black")
+      .strokeWidth(2)
+      .fill("white")
+      .drawText(0, 0, whom, "center")
+      .stream('gif', function (err, stdout, stderr) {
+         if (err) return next(err);
+         res.set('Content-Type', 'image/gif');
+         stdout.pipe(res);
+         stdout.pipe(file);
+         stdout.on('error', next);
+       });
+  } else {
+    var file = fs.createReadStream(cacheFile);
+    res.set('Content-Type', 'image/gif');
+    file.pipe(res);
+  }
 });
 
 module.exports = router;
