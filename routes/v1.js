410a84dfaa9795f8f6c9cabaa857480c88e15b96
Andrew Matheny <andrew.j.matheny@gmail.com>
2016-04-07 15:39:42 -0400

    ES6

--- routes/v1.js
+++ routes/v1.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var ImageGen = require(__dirname + '/../lib/image_generator')
+var ImageGen = require(__dirname + '/../lib/image_generator');
 
 var fs = require('fs');
 var gm = require('gm'),
@@ -9,21 +9,21 @@ var bunyan = require('bunyan');
 var log = bunyan.createLogger({name: "apiV1"});
 var tempfile = require('tempfile');
 
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   var whom = req.url.substring(req.url.indexOf('?') + 1, req.url.length);
   whom = unescape(whom);
   whom = whom.replace(/\+/g, " ");
 
-  var filename = __dirname + '/../public/gifs/liquor.gif';
+  var filename = `${__dirname}/../public/gifs/liquor.gif`;
   var streamPromise = ImageGen.getAnnotatedImage(whom, filename);
 
-  var annotateWin = function(stream) {
+  var annotateWin = (stream) => {
     res.set('Content-Type', 'image/gif');
     stream.pipe(res);
   };
-  var annotateFail = function(err) {
-    log.error({err: err}, "Unable to annotate image " + filename + " with text '" + whome + "'");
-    res.send(500, { error: 'something blew up' });
+  var annotateFail = (err) => {
+    log.error({err: err}, `Unable to annotate image '${filename}' with text '${whom}'`);
+    res.status(500).send({ msg: 'something blew up', err: err });
   };
   streamPromise.then(annotateWin, annotateFail);
 });
